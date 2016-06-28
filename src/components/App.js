bd8d838701a0103c7fad8728d3101ef170dfaf9c
Yu Chi-Shan <chsyu@tea.ntue.edu.tw>
2016-06-28 13:57:20 +0800

    ytsearch

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
 import '../assets/stylesheets/base.scss';
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search_bar';
+import VideoList from './video_list';
+import VideoDetail from './video_detail';
+const API_KEY = 'AIzaSyBEecjE_v8D5bC5nrfS3Xm-heojWG26L0w';
 
-const Hello = React.createClass({
-  render() {
-    return(
-      <h1>Hello, {this.props.name}!</h1>
-    )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('ntue dtd');
   }
-});
 
-export default Hello;
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term=>this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+export default App;
