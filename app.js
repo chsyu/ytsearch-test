5ed80f0d2180a20d39db6a70a84f22071aa37bcc
Yu Chi-Shan <chsyu@tea.ntue.edu.tw>
2016-06-28 15:34:45 +0800

    final

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var app = express();
 var PORT = process.env.PORT || 8080
 
 // using webpack-dev-server and middleware in development environment
+if(process.env.NODE_ENV !== 'production') {
+  var webpackDevMiddleware = require('webpack-dev-middleware');
+  var webpackHotMiddleware = require('webpack-hot-middleware');
+  var webpack = require('webpack');
+  var config = require('./webpack.config');
+  var compiler = webpack(config);
+
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
+}
 
 app.use(express.static(__dirname + '/dist'));
 
